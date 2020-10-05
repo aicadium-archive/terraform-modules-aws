1c367ad979fe7b8701eb798290b1e4b88e006881
Yong Wen Chua <lawliet89@users.noreply.github.com>
2020-10-05 17:06:52 +0800

    Update outputs.tf

--- modules/terraform_kms/outputs.tf
+++ modules/terraform_kms/outputs.tf
@@ -10,10 +10,10 @@ output "terraform_key_id" {
 
 output "all_keys_policy" {
   description = "Name of the All Keys policy"
-  value       = aws_iam_policy.all_keys_admin.name
+  value       = var.create_all_keys_policy ? aws_iam_policy.all_keys_admin[0].name : null
 }
 
 output "all_keys_policy_arn" {
   description = "ARN of the All Keys policy"
-  value       = aws_iam_policy.all_keys_admin.arn
+  value       = var.create_all_keys_policy ? aws_iam_policy.all_keys_admin[0].arn : null
 }
